b1e360af2d4a0f18a96cf6bbdd5ad43c4d32dfd5
AmizukiZ <3073877601@qq.com>
2025-03-06 22:29:56 +0800

    创建book模块用到的dto, 添加了class-validator校验规则装饰器, multer实现文件上传, 自定义storage

---
+++ src/book/book.service.ts
@@ -0,0 +1,115 @@
+import {
+    BadRequestException,
+    Injectable,
+} from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { DbService } from 'src/db/db.service';
+import { Book } from './entities/book.entity';
+
+function randomNum() {
+    return Math.floor(Math.random() * 1000000);
+}
+
+@Injectable()
+export class BookService {
+    constructor(
+        private readonly dbService: DbService,
+    ) {}
+
+    //获取图书列表
+    async list() {
+        const books = await this.dbService.read();
+        return books;
+    }
+
+    //根据id获取图书
+    async findById(id: number) {
+        const books: Book[] =
+            await this.dbService.read();
+        return books.find(
+            (book) => book.id === id,
+        );
+    }
+
+    //创建图书
+    async create(createBookDto: CreateBookDto) {
+        const books: Book[] =
+            await this.dbService.read();
+
+        const book = new Book();
+        book.id = randomNum();
+        book.author = createBookDto.author;
+        book.name = createBookDto.name;
+        book.description =
+            createBookDto.description;
+        book.cover = createBookDto.cover;
+
+        books.push(book);
+
+        try {
+            await this.dbService.write(books);
+            console.log(
+                '图书创建成功: ',
+                book.description,
+            );
+            return book;
+        } catch (error) {
+            throw new BadRequestException(
+                '图书创建失败',
+            );
+        }
+    }
+
+    //更新图书
+    async update(updateBookDto: UpdateBookDto) {
+        const books: Book[] =
+            await this.dbService.read();
+
+        const foundBook = books.find((book) => {
+            console.log(
+                book.id,
+                +updateBookDto.id,
+            );
+            console.log(
+                book.id === +updateBookDto.id,
+                updateBookDto instanceof
+                    UpdateBookDto,
+            );
+            return book.id === +updateBookDto.id;
+        });
+
+        if (!foundBook) {
+            throw new BadRequestException(
+                '图书不存在',
+            );
+        }
+
+        foundBook.name = updateBookDto.name;
+        foundBook.description =
+            updateBookDto.description;
+        foundBook.cover = updateBookDto.cover;
+
+        await this.dbService.write(books);
+        return foundBook;
+    }
+
+    //删除图书
+    async delete(id: number) {
+        const books: Book[] =
+            await this.dbService.read();
+        const index = books.findIndex(
+            (book) => book.id === id,
+        );
+
+        if (index !== -1) {
+            books.splice(index, 1);
+            await this.dbService.write(books);
+            return '图书删除成功';
+        }
+
+        throw new BadRequestException(
+            '图书不存在',
+        );
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
     const app =
-        await NestFactory.create(
+        await NestFactory.create<NestExpressApplication>(
             AppModule,
         );
+
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
             transform: true,
         }),
     );
-    await app.listen(
-        process.env.PORT ??
-            3000,
+
+    app.enableCors({
+        origin: '*',
+        //设置为false，表示不使用cookie
+        credentials: false,
+    });
+
+    app.useStaticAssets(
+        join(__dirname, '../uploads'),
+        { prefix: '/uploads' },
     );
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
